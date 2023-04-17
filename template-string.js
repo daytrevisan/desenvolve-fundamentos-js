061d55872838e6b06d3e1b50bc1245891d42c09d
Dayane <dayanetm@outlook.com>
2023-04-17 19:46:00 -0300

    js-fundamentos

---
+++ template-string.js
@@ -0,0 +1,8 @@
+const nome = "Maria";
+const idade = 2023-1998;
+const cidadeDeNascimento = "Campinas";
+
+//console.log("Meu nome é " + nome + ", tenho " + idade + " anos e nasci em " + cidadeDeNascimento);
+
+// Template String > utiliza acento grave/crase
+console.log(`Meu nome é ${nome}, tenho ${idade} e nasci em ${cidadeDeNascimento}`);
