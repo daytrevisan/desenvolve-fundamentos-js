18e27eff21a056a7c106d818926858d481d63a96
Dayane <dayanetm@outlook.com>
2023-04-23 20:49:52 -0300

    js-arrays

--- array-for-media.js
+++ array-for-media.js
@@ -1,10 +1,14 @@
 // Neste algoritmo, iremos calcular a média de uma lista de números utilizando o laço FOR
+// Calcular a média entre as seguintes notas: 10, 6.5, 8 e 7.5
 
 const notas = [10, 6.5, 8, 7.5];
 
 let somaDasNotas = 0;
 
 for(let i = 0; i < notas.length; i++) {
-    somaDasNotas = somaDasNotas + notas[i];
-    return (somaDasNotas/notas.length)
+    somaDasNotas += notas[i]
 }
+
+let mediaDasNotas = somaDasNotas/notas.length;
+
+console.log(mediaDasNotas)
