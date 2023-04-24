a0d54f18f85c16c78e70bfc20e5736e5d391ca00
Dayane <dayanetm@outlook.com>
2023-04-24 12:10:07 -0300

    js-arrays

--- array-juntando-turmas.js
+++ array-juntando-turmas.js
@@ -1,7 +1,10 @@
-// Método CONCAT
-// Destinado a unificar dois arrays
+// Método CONCAT()
+// Utilizado para concatenar um array com outro
+
 // Mencionamos um dos arrays passando o método e passa o segundo array como parâmetro
 // Importante: deve-se criar uma NOVA CONST para salvar o resultado
+
+// Desafio: Juntar as diferentes turmas em uma turma única
 
 const salaDePython = ['Melissa', 'Helena', 'Rodrigo'];
 const salaDeJS = ['Ju', 'Leo', 'Raquel'];
