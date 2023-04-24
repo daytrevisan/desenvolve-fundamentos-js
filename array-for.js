ad7a37821edef23a18a65f894467abe7107160f7
Dayane <dayanetm@outlook.com>
2023-04-24 12:09:53 -0300

    js-arrays

--- array-for.js
+++ array-for.js
@@ -1,14 +1,16 @@
-// FOR
+// Laço de Repetição FOR
+// O laço de repetição FOR é utilizado para trechos de código que se repetem mediante uma condição
+// A condição do meio é a responsável pelo término do código ou por mantê-lo em repetição de execução 
+
 // Desafio: mostrar uma lista de números com os respectivos índices do array
 
 const lista = [100, 200, 300, 400, 500, 600];
 
-// O laço de repetição FOR é utilizado para trechos de código que se repetem mediante uma condição
-// A condição do meio é a responsável pelo término do código ou por mantê-lo em repetição de execução 
-
 for(let i = 0; i < lista.length; i++) {
     console.log(i, lista[i])
 }
+
+// ---------------------------------------------------------------------------------------------------------
 
 // Condições para loop infinito
 // * Falta da condição de parada (parâmetro central)
