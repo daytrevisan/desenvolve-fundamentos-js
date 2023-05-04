6564e31d50e1aac914797fb75804488242390b1c
Dayane <dayanetm@outlook.com>
2023-05-03 23:33:02 -0300

    js-arrays

--- obj-spread-operator.js
+++ obj-spread-operator.js
@@ -1,4 +1,4 @@
-//
+// Unificando dois ou mais objetos
 
 const fichaGuerreiro = {
     nome: 'Aragon',
@@ -10,16 +10,26 @@ const equipoGuerreiro = {
     capa: 'capa élfica +2'
 }
 
+// Para juntar dois ou mais arrays, referenciamos cada array dentro de um array maior
 const guerreiro1 = [fichaGuerreiro, equipoGuerreiro];
 console.log(guerreiro1);
+
+// Saída no terminal
 
 // [
 //     { nome: 'Aragon', classe: 'guerreiro' },
 //     { espada: 'Andúril', capa: 'capa élfica +2' }
 // ]
 
+// Neste caso como usamos esta ação para objeto, o resultado serão dois objetos distintos
+
+// Mas e se quisermos juntar as chaves dos objetos em um só objeto?
+// Fazemos a mesma ação, mas passando chaves e adicionando o operador de espalhamento(...)
+
 const guerreiro2 = {...fichaGuerreiro, ...equipoGuerreiro};
 console.log(guerreiro2);
+
+// Saída no terminal
 
 // {
 //   nome: 'Aragon',
@@ -27,6 +37,8 @@ console.log(guerreiro2);
 //   espada: 'Andúril',
 //   capa: 'capa élfica +2'
 // }
+
+// Agora sim temos apenas um objeto que engloba todas as chaves quantos seus respectivos valores!
 
 // --------------------------------------------------------------------------------------------------------
 
@@ -50,3 +62,6 @@ const mago = {
 
    const personagens = {...mago, ...guerreiro, ...ranger};
    console.log(personagens); // { nome: 'Legolas', classe: 'ranger' }
+
+   // Nota-se que apenas o último objeto foi considerado
+   // Com o mesmo nome de chaves, os valores foram sobrescritos
