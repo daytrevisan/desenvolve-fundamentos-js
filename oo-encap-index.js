bc7a8cc11f3e22f028b4f165bae505b7155d8d11
Dayane <dayanetm@outlook.com>
2023-06-19 22:52:36 -0300

    js-oo

---
+++ oo-encap-index.js
@@ -0,0 +1,22 @@
+import User from "./oo-encap-User.js";
+import Docente from "./oo-encap-Docente.js";
+import Admin from "./oo-encap-Admin.js";
+
+const novoUser = new User('Mariana', 'm@m.com', '2000-01-01')
+console.log(novoUser.exibirInfos())
+
+// Deste modo, o nome pode ser acessado e alterado
+
+// novoUser.#nome = 'Márcia'
+// console.log(novoUser.exibirInfos())
+
+// Acessando o método privado #montaObjUser
+
+// console.log(novoUser.#montaObjUser())
+
+// O acesso não é permitido, pois não é possível acessar o método por fora da classe
+
+console.log(novoUser.exibirInfos())
+
+const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2000-01-01')
+console.log(novoAdmin.exibirInfos())
